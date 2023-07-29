4f71194248a976c223988b05516d36084d89d645
KossAll <Sasssko85@ukr.net>
2023-07-29 14:34:00 +0300

    new

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './js/anchor-scroll';
-import './js/mobile-menu';
 import './js/header-scroll';
 import './js/simpleLightbox';
-<<<<<<< Updated upstream
-
-=======
 import './js/gallery/category1';
->>>>>>> Stashed changes
